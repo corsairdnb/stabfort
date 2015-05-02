aa993e7e6a4c50217f26b155912933cc485d55f1
corsairdnb <dmitry.rychkov@gmail.com>
2015-05-03 01:02:15 +0300

    setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,30 @@
 var gulp = require('gulp'),
 	less = require('gulp-less'),
 	plumber = require('gulp-plumber'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sourcemaps = require('gulp-sourcemaps'),
+	print = require('gulp-print');
 
 var lessPath = 'less/',
-	lessImportsPath = lessPath + 'imports/',
-	lessMainPath = lessPath + 'stabfort/main/**/*',
-	bootstrapPath = 'less/bootstrap/less/',
+	lessMainPath = lessPath + '*.less',
 	cssOutputPath = 'css/';
-
-gulp.task('less', function () {
-	return gulp.src(lessMainPath)
-		.pipe(less({
-			paths: [bootstrapPath, lessImportsPath]
-		}))
-		.pipe(gulp.dest(cssOutputPath))
-});
 
 gulp.task('watch', function () {
 	return gulp.src(lessMainPath)
 		.pipe(watch(lessMainPath))
 		.pipe(plumber())
-		.pipe(less({
-			paths: [bootstrapPath]
+		.pipe(print(function(filepath) {
+			var date = new Date(),
+				dateformat = require('gulp/node_modules/gulp-util/node_modules/dateformat');
+			return dateformat(date, 'HH:MM:ss') + ' ' + filepath;
 		}))
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(autoprefixer({
+			browsers: ['Android >= 4', 'Chrome >= 20', 'Firefox >= 15', 'Explorer >= 8', 'iOS >= 6', 'Opera >= 15'],
+			cascade: false
+		}))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(cssOutputPath))
 });
