28591635a913206894ae7d643c52b405ab4cd62c
corsairdnb <dmitry.rychkov@gmail.com>
2015-06-01 01:58:22 +0300

    home

--- js/stabfort.js
+++ js/stabfort.js
@@ -1,21 +1,65 @@
 $(function(){
 
+	/*
+	* данный код предназначен только для демострации
+	* */
+
 	$('.mobile-icon').on('click', function(){
 		$('.top-menu').toggleClass('opened');
 	});
 
+	$('.mobile-icon, .top-menu ul li a').on('click', function(e){
+		e.stopPropagation();
+		e.preventDefault();
+	});
+	$('body').on('click', function(e){
+		$('.top-menu').removeClass('opened');
+	});
+
+	$('#trigger-level-2').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.services-list').addClass('level-2-active');
+		$('.services-list-level-2').addClass('active');
+		$('.level-1-overlay').show();
+	});
+	$('.services-list-level-1, .services-list-level-2-back, .services-list-level-2-close').on('click', function(){
+		$('.services-list').removeClass('level-2-active level-3-active');
+		$('.services-list-level-2').removeClass('active');
+		$('.services-list-level-3').removeClass('active');
+		$('.level-2-item').removeClass('active');
+		$('.level-1-overlay').hide();
+	});
+	$('.services-list-level-3-close').on('click', function(){
+		$('.services-list').removeClass('level-3-active');
+		$('.level-2-item').removeClass('active');
+		$('.services-list-level-3').removeClass('active');
+	});
+	$('.tile-list-link, .tile-header-link').on('click', function(e){
+		e.preventDefault();
+	});
+
+	$('.level-2-item').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.level-2-item').removeClass('active');
+		$(this).addClass('active');
+		$('.services-list').addClass('level-3-active');
+		$('.services-list-level-3').addClass('active');
+	});
+
 	$('.peppermint').Peppermint({
 		//transition time when changing slides, ms
-		speed: 300,
+		speed: 500,
 
 		//transition time when changing slides after touch, ms
 		touchSpeed: 300,
 
 		//slideshow enabled
-		slideshow: false,
+		slideshow: true,
 
 		//slideshow interval, ms
-		slideshowInterval: 2000,
+		slideshowInterval: 5000,
 
 		//stop slideshow after user interacts with the slider
 		stopSlideshowAfterInteraction: false,
