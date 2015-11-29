c90eb7311d37f13cb6e653e4bf9451a6f3b524ba
corsairdnb <dmitry.rychkov@gmail.com>
2015-11-29 23:32:08 +0300

    gulp svg sprite

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	autoprefixer = require('gulp-autoprefixer'),
 	sourcemaps = require('gulp-sourcemaps'),
-	csso = require('gulp-csso');
+	csso = require('gulp-csso'),
+	svgSprite = require('gulp-svg-sprite');
 
 gulp.task('less', function () {
 	return gulp
@@ -16,6 +17,34 @@ gulp.task('less', function () {
 		.pipe(sourcemaps.write())
 		//.pipe(csso(true))
 		.pipe(gulp.dest('css/'))
+});
+
+var svgSpriteConfig                  = {
+	shape               : {
+		dimension       : {         // Set maximum dimensions
+			maxWidth    : 32,
+			maxHeight   : 32
+		},
+		spacing         : {         // Add padding
+			padding     : 10
+		},
+		//dest            : 'out/intermediate-svg'    // Keep the intermediate files
+	},
+	mode                : {
+		view            : {         // Activate the «view» mode
+			bust        : false,
+			render      : {
+				less    : true      // Activate Sass output (with default options)
+			}
+		},
+		symbol          : true      // Activate the «symbol» mode
+	}
+};
+
+gulp.task('svg', function () {
+	gulp.src('img/svg/*.svg')
+		.pipe(svgSprite(svgSpriteConfig))
+		.pipe(gulp.dest('img/svg_sprites'));
 });
 
 gulp.task('watch', function () {
