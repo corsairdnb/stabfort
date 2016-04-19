44a71201da9ef2a770f37e283d96fa3fdc7c6bc5
corsairdnb <dmitry.rychkov@gmail.com>
2016-04-19 23:00:31 +0300

    home variation

---
+++ js/home-3.js
@@ -0,0 +1,94 @@
+$(function(){
+
+	/*
+	* данный код предназначен только для демострации
+	* */
+
+	$('.mobile-icon').on('click', function(){
+		$('.top-menu').toggleClass('opened');
+	});
+
+	$('.mobile-icon, .top-menu ul li a').on('click', function(e){
+		e.stopPropagation();
+		e.preventDefault();
+	});
+	$('.wrapper').on('click', function(e){
+		$('.top-menu').removeClass('opened');
+		$('.search-output-item').tooltip('hide');
+	});
+
+	$('#trigger-level-2').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.services-list').addClass('level-2-active');
+		$('.services-list-level-2').addClass('active');
+		$('.level-1-overlay').show();
+	});
+	$('.services-list-level-1, .services-list-level-2-back, .services-list-level-2-close').on('click', function(){
+		$('.services-list').removeClass('level-2-active level-3-active');
+		$('.services-list-level-2').removeClass('active');
+		$('.services-list-level-3').removeClass('active');
+		$('.level-2-item').removeClass('active');
+		$('.level-1-overlay').hide();
+	});
+	$('.services-list-level-3-close').on('click', function(){
+		$('.services-list').removeClass('level-3-active');
+		$('.level-2-item').removeClass('active');
+		$('.services-list-level-3').removeClass('active');
+	});
+	$('.tile-list-link, .tile-header-link').on('click', function(e){
+		e.preventDefault();
+	});
+
+	$('.level-2-item').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.level-2-item').removeClass('active');
+		$(this).addClass('active');
+		$('.services-list').addClass('level-3-active');
+		$('.services-list-level-3').addClass('active');
+	});
+
+	$('.home-video-play').on('click', function(){
+		//TODO: реализовать остановку видео при закрытии модального окна
+		// из-за same-origin-policy невозможно получить доступ к плееру
+		$('.video-modal')
+			.on('show.bs.modal', function () {
+				var body = $(this).find('.modal-body');
+				if (body.html() == '') {
+					body.html('<iframe width="850" height="638" src="http://www.youtube.com/embed/3zbvUTQq6Uc?rel=0&autoplay=true" frameborder="0" allowfullscreen></iframe>');
+				}
+			})
+			/*.on('hide.bs.modal', function () {
+				var iframe = document.getElementById(player_id).contentWindow;
+				iframe.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+			})*/
+			.modal();
+	});
+
+	$('.home-services-more').on('click', function(){
+		$(this).toggleClass('triggered');
+	});
+
+	$('.services-filter-type-list a').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$(this).parent().addClass('active').siblings().removeClass('active');
+	});
+
+	$('.search-output-item')
+		.on('click', function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			$('.search-output-item').tooltip('hide');
+		})
+		.tooltip({
+			container: '.search-brief-output',
+			html: true,
+			title: 'Пройдите <a href="#">регистрацию</a> и получите доступ к заявкам',
+			trigger: 'click'
+		});
+
+
+
+});
